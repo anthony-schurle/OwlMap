ed39daa4fa45eefcedc7a7e9ed1f7f57fc6d7e07
anthony-schurle <as556@rice.edu>
2025-09-20 14:03:11 -0500

    Backend Update

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
 from . import main
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="/app/frontend"), name="static")
+
 interface = main.Main()
 
 @app.get("/nodes")
-def navigate():
+def get_nodes():
     return {"nodes": interface.get_nodes()}
 
 @app.get("/navigate")
-def navigate(start_str, end_str):
+def navigate(start_str: str, end_str: str):
     path, distance = interface.navigate(start_str, end_str)
 
-    return {"path": path, "distance": distance}
+    return {"path": [node.name for node in path], "distance": distance}
