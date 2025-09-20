d551c76388489afe4858c09a097c2c5e23c8c934
anthony-schurle <as556@rice.edu>
2025-09-20 17:36:29 -0500

    Fixes

--- app/backend/app.py
+++ app/backend/app.py
@@ -4,7 +4,12 @@ from fastapi.responses import FileResponse
 
 from backend.logic import main
 
+from backend.data.crud import init
+from backend.data.database import engine, Base
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.mount("/frontend/dist", StaticFiles(directory="frontend/dist"), name="frontend-dist")
 
@@ -17,7 +22,7 @@ def index():
 @app.get("/nodes")
 def get_nodes():
     nodes = interface.get_nodes()
-    return {"nodes": [(node.name, node.latitude, node.longitude) for node in nodes]}
+    return {"nodes": [(node.name, node.x, node.y) for node in nodes]}
 
 @app.get("/navigate")
 def navigate(start_str: str, end_str: str):
--- app/backend/data/crud.py
+++ app/backend/data/crud.py
@@ -1,9 +1,29 @@
-from sqlalchemy.orm import SessionLocal
-from backend.data import models
+from sqlalchemy.orm import Session
+from backend.data.database import SessionLocal
+from backend.data.models import Location
 
 def init():
     locations = [
-        {}
-    ]
+    Location(name="Recreation Center", latitude=29.718234287094702, longitude=-95.40365381464878),
+    Location(name="Fondren Library", latitude=29.7184889244895, longitude=-95.40003002999113),
+    Location(name="Lovett College", latitude=29.716736984405465, longitude=-95.39799155165602),
+    Location(name="Will Rice College", latitude=29.716466139816475, longitude=-95.39871387154595),
+    Location(name="Hanszen College", latitude=29.715892410957814, longitude=-95.40018766506088),
+    Location(name="Sid Richardson College", latitude=29.715691758825585, longitude=-95.39829300944861),
+    Location(name="Wiess College", latitude=29.714691511356488, longitude=-95.40085670596993),
+    Location(name="Baker College", latitude=29.71733243453455, longitude=-95.39902187616832),
+    Location(name="Duncan College", latitude=29.72188855408422, longitude=-95.39846252369836),
+    Location(name="Martel College", latitude=29.72179538030585, longitude=-95.39770614078927),
+    Location(name="Jones College", latitude=29.72165096077053, longitude=-95.39676200326267),
+    Location(name="Brown College", latitude=29.72158108027819, longitude=-95.39587687430644),
+    Location(name="McMurtry College", latitude=29.720369810681373, longitude=-95.39811383654994),
+]
+
+    db = SessionLocal()
+
+    for loc in locations:
+        db.add(loc)
+        db.commit()
+        db.refresh(loc)
 
     
